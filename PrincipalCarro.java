eca38790484e58e81c245df29280a82c4cd797c2
ErikaChristiny <erika.christiny@academico.ifpb.edu.br>
2025-05-07 11:26:05 -0300

    Add files via upload

---
+++ PrincipalCarro.java
@@ -0,0 +1,10 @@
+public class PrincipalCarro {
+
+    public static void main(String[]args){
+        Carro c = new Carro("Ford", "ka", 2015);
+
+        c.acelerar();
+        c.frear();
+
+    }
+}
---
+++ PrincipalRetangulo.java
@@ -0,0 +1,8 @@
+public class PrincipalRetangulo {
+
+    public static void main(String[]args){
+        Retangulo r = new Retangulo(4, 8);
+        r.imprimir();
+    }
+
+}
